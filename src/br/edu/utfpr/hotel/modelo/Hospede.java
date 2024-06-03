fc6f84915f5eb4ab8141be20e0fcc1166ede9ae1
Joao Victor Rosada Yamaoka <joaozinhory@hotmail.com>
2024-06-03 14:41:07 -0300

    Retirada do atributo Idade

--- src/br/edu/utfpr/hotel/modelo/Hospede.java
+++ src/br/edu/utfpr/hotel/modelo/Hospede.java
@@ -2,7 +2,6 @@ package br.edu.utfpr.hotel.modelo;
 
 public class Hospede {
     private String nome;
-    private int idade;
     private String endereco;
     private String telefone;
     private String email;
@@ -12,9 +11,8 @@ public class Hospede {
     }
 
 
-    public Hospede(String nome, int idade, String endereco, String telefone, String email) {
+    public Hospede(String nome, String endereco, String telefone, String email) {
         this.nome = nome;
-        this.idade = idade;
         this.endereco = endereco;
         this.telefone = telefone;
         this.email = email;
@@ -29,13 +27,7 @@ public class Hospede {
         this.nome = nome;
     }
 
-    public int getIdade() {
-        return idade;
-    }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 
     public String getEndereco() {
         return endereco;
@@ -66,7 +58,6 @@ public class Hospede {
     public String toString() {
         return "Hospede{" +
                 "nome='" + nome + '\'' +
-                ", idade=" + idade +
                 ", endereco='" + endereco + '\'' +
                 ", telefone='" + telefone + '\'' +
                 ", email='" + email + '\'' +
